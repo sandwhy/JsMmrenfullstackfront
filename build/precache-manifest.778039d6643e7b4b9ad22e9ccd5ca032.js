ae3d2f0b6d71fc1eb6a42988a8ba4064985713c8
sandwhy <83949640+sandwhy@users.noreply.github.com>
2022-12-20 16:57:55 +0700

    pushing build to github build branch

---
+++ build/precache-manifest.778039d6643e7b4b9ad22e9ccd5ca032.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45ef6e99eafd3efe6d57ae671383ee32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f041423d29540459e0ba",
+    "url": "/static/css/main.bcedd86b.chunk.css"
+  },
+  {
+    "revision": "b8bb5b23edbd01bb74b4",
+    "url": "/static/js/2.1719556a.chunk.js"
+  },
+  {
+    "revision": "eca373d6688a5d5efead34244f081e8b",
+    "url": "/static/js/2.1719556a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f041423d29540459e0ba",
+    "url": "/static/js/main.3bca2934.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.3bca2934.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  },
+  {
+    "revision": "8e9763ff2bd5c0571f6be83d85310d40",
+    "url": "/static/media/memories-Logo.8e9763ff.png"
+  },
+  {
+    "revision": "6c7b5ec0184b26d4b93f36ec813156b3",
+    "url": "/static/media/memories-Text.6c7b5ec0.png"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
